aa5fb24a55c712471dca7d6bae2791a655d2df5e
Arwid Thornstrom <arwidt@gmail.com>
2016-07-01 17:08:02 +0200

    working on tests for new usage interface

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var tto = require('terminal-table-output');
 var _timer = (function() {
 
     var _groups = {};
-    _groups['default'] = {
-        id: 'default',
-        timers: []
-    };
 
     var _outputTime = function(t) {
         switch(true) {
@@ -67,8 +63,71 @@ var _timer = (function() {
         }
 
     })();
+    
+    var _createGroup = function(opts) {
+        var _opts = opts || {};
+        var _timers = [];
+        var _inst = {
+            get opts() {
+                return _opts;
+            },
+            
+            // Stops all timers and
+            // flush all data and timers
+            flush: function() {
+                
+            },
+            
+            // Prints the group in a specific style
+            print: function(type) {
+                // var group = _groups[groupId || "default"],
+                //     timers = group.timers,
+                //     total = 0;
+                //
+                // group.tto.pushrow(["Name", "Time     ", "Percent  "])
+                //     .line();
+                //
+                // var i, len, row;
+                // for (i = 0, len = timers.length; i < len; i++) {
+                //     total += timers[i].time;
+                // }
+                // for (i = 0, len = timers.length; i < len; i++) {
+                //     row = timers[i].row;
+                //     row.push(Math.round((timers[i].time/total)*100) + " %");
+                //     group.tto.pushrow(row);
+                // }
+                // group.tto.line("-");
+                // group.tto.pushrow(["Total:", _outputTime(total), "100%"]);
+                //
+                // group.tto.print(true);    
+            },
+            
+            // Gets a timer by id in the scope
+            // of this group.
+            // If no timer is in this scope
+            // one will be created.
+            timer: function(id) {
+                if (_timers.hasOwnProperty(id)) {
+                    return _timers[id];
+                }
+                _timers[id] = _createTimer({
+                    id: id,
+                    startTime: _now(),
+                    endTime: null,
+                    totalTime: null
+                });
+                return _timers[id];
+            },
+            
+            // Stops all active timers.
+            stop: function() {
+                
+            }
+        };
+        return _inst;
+    };
 
-    var _getTimer = function(_opts) {
+    var _createTimer = function(_opts) {
         var _opts = _opts || {};
         var _inst = {
             get opts() {
@@ -105,103 +164,65 @@ var _timer = (function() {
         return _inst;
     };
 
+    _groups['default'] = _createGroup('default');
+
     var _inst = {
         public_scope: {
             now: _now,
-            groups: _groups, 
-            getTimer: _getTimer,
-            outputTime: _outputTime
+            groups: _groups
         },
-
-        createGroup: function(id) {
+        
+        // groups should create or get a group by a id.
+        group: function(id) {
             if (_groups.hasOwnProperty(id)) {
-                //console.warn(id + ' is taken as a groupId, please use another!');
-                return this;
+                return _groups[id];
             }
-            _groups[id] = {
-                id: id,
-                timers: [],
-                tto: tto.create()
-            };
-            return this;
+            _groups[id] = _createGroup({
+                id: id
+            });
+            return _groups[id];
         },
 
-        printGroup: function(groupId) {
-            //console.log("printGroup:", groupId);
-
-            var group = _groups[groupId || "default"],
-                timers = group.timers,
-                total = 0;
-
-            group.tto.pushrow(["Name", "Time     ", "Percent  "])
-                .line();
-
-            var i, len, row;
-            for (i = 0, len = timers.length; i < len; i++) {
-                total += timers[i].time;
-            }
-            for (i = 0, len = timers.length; i < len; i++) {
-                row = timers[i].row;
-                row.push(Math.round((timers[i].time/total)*100) + " %");
-                group.tto.pushrow(row);
-            }
-            group.tto.line("-");
-            group.tto.pushrow(["Total:", _outputTime(total), "100%"]);
-
-            group.tto.print(true);
-
-            return this;
-        },
-
-        flush: function() {
-            //console.log("Flush all groups and timers");
-            _groups = [];
-        },
-
+        // shorthand function to get a timer.
+        // gets a timer by id, regardless of group
+        // if more that one timer is created with the same name
+        // an array with timers will be returned.
+        // groupId is optional, if timerId has no
+        // match in any group, it will be created in
+        // the default group.
         timer: function(timerId, groupId) {
             //console.log("startTimer:", timerId, groupId);
 
             // There is a timer
-            var group = _groups[groupId];
-            if (group) {
-                var timers = _groups[groupId || "default"].timers;
-                for (var i = 0, len = timers.length; i < len; i++) {
-                    if (timers[i].opts.id === timerId) {
-                        return timers[i];
-                    }
-                }
-            }
-
-            var timers = _groups[groupId || "default"].timers;
-            for (var i = 0, len = timers.length; i < len; i++) {
-                if (timers[i].opts.id === timerId) {
-                    return timers[i];
-                }
-            }
-
-            if (!_groups.hasOwnProperty(groupId)) {
-                _inst.createGroup(groupId);
-            }
-            var timer = _getTimer({
-                id: timerId,
-                startTime: _now(),
-                endTime: null,
-                totalTime: null
-            });
-            timer.start();
-            _groups[groupId || "default"].timers.push(timer);
-            return timer;
-        },
-
-        endTimer: function(timerId, groupId) {
-            //console.log("endTimer:", timerId);
-            var timers = _groups[groupId || "default"].timers;
-            for (var i = 0, len = timers.length; i < len; i++) {
-                if (timers[i].opts.id === timerId) {
-                    timers[i].stop();
-                    return this;
-                }
-            }
+            // var group = _groups[groupId];
+            // if (group) {
+            //     var timers = _groups[groupId || "default"].timers;
+            //     for (var i = 0, len = timers.length; i < len; i++) {
+            //         if (timers[i].opts.id === timerId) {
+            //             return timers[i];
+            //         }
+            //     }
+            // }
+            //
+            // var timers = _groups[groupId || "default"].timers;
+            // for (var i = 0, len = timers.length; i < len; i++) {
+            //     if (timers[i].opts.id === timerId) {
+            //         return timers[i];
+            //     }
+            // }
+            //
+            // if (!_groups.hasOwnProperty(groupId)) {
+            //     _inst.createGroup(groupId);
+            // }
+            // var timer = _getTimer({
+            //     id: timerId,
+            //     startTime: _now(),
+            //     endTime: null,
+            //     totalTime: null
+            // });
+            // timer.start();
+            // _groups[groupId || "default"].timers.push(timer);
+            // return timer;
         }
     };
 
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var timereport = require('../index.js');
+var tr = require('../index.js');
 var colors = require('colors');
 var should = require('should');
 
@@ -10,9 +10,9 @@ describe('TimeReport.js', function() {
 
         it('should return correct time', function(done) {
 
-            var t = timereport.public_scope.now();
+            var t = tr.public_scope.now();
             setTimeout(function() {
-                var _t = ~~(timereport.public_scope.now() - t);
+                var _t = ~~(tr.public_scope.now() - t);
                 if (_t > 990 && _t < 1010) {
                     _t = 1000;
                 }
@@ -24,113 +24,148 @@ describe('TimeReport.js', function() {
 
     });
 
-    describe('Public functions', function() {
-
-        it('should return itself when running specific functions', function() {
-            should(timereport.createGroup('test')).equal(timereport);
-            should(timereport.printGroup('test')).equal(timereport);
-            should(timereport.startTimer('testtimer', 'test')).equal(timereport);
-            should(timereport.endTimer('testtimer', 'test')).equal(timereport);
-        });
-
-    });
-
-    describe('Output-time', function() {
-
-        it('should return x ms when less then 1 s', function() {
-            should(timereport.public_scope.outputTime(456)).equal("456 ms");
-        });
-        it('should return x s when more than 1 s and less than 1 m', function() {
-            should(timereport.public_scope.outputTime(23123)).equal("23.123 s");
-        });
-        it('should return x m and x s when more than 1 m but less than 1 h', function() {
-            should(timereport.public_scope.outputTime(60000*3+12345)).equal("3 m and 12.345 s");
-        });
-        it('should return a funny text because you really should not use this on that long processes.', function() {
-            should(timereport.public_scope.outputTime(60000*123), "looong time!");
-        });
-
-    });
-
     describe('Groups', function() {
 
-        it('should create a new group when it is not created before', function() {
-            timereport.createGroup('testgroup1');
-            should(timereport.public_scope.groups['testgroup1'].id).equal('testgroup1');
+        it('should have a default group setup from start named default', function() {
+            should.exist(tr.public_scope.groups['default']);
         });
 
-        it('should be possible to ad a color from colors string prototype as a base color', function() {
-           timereport.setColor(colors.red, 'testgroup1'); 
+        it('should create a group if no group with that id is created before', function() {
+            var group = tr.group('g1');
+            group.should.have.ownProperty('opts');
+            group.opts.id.should.equal('g1');
         });
 
-        it('should create a group from startTimer functions if that group is not created before', function() {
-            timereport.startTimer('grouptimer1', 'specialgroup1');
-
-            timereport.endTimer('grouptimer1', 'specialgroup1');
-
-            should.exist(timereport.public_scope.groups['specialgroup1']);
-        });
-
-        it('should not create a new group if a group is already created', function() {
-            timereport.startTimer('testtimer', 'testgroup1');
-            timereport.startTimer('testtimer2', 'testgroup1');
-
-            timereport.endTimer('testtimer', 'testgroup1');
-            timereport.endTimer('testtimer2', 'testgroup1');
-
-            timereport.createGroup('testgroup1');
-
-            should(timereport.public_scope.groups['testgroup1'].timers.length).equal(2);
+        it('should return the group if a group with that id is created before', function() {
+            tr.group('g1').should.equal(tr.public_scope.groups['g1']);
         });
 
     });
 
     describe('Timers', function() {
 
-        it('should start a timer when a timer is started', function(done) {
-            timereport.startTimer('timer1', 'group1');
-
-            setTimeout(function() {
-                timereport.startTimer('timer2', 'group1');
-            }, 100);
-
-            setTimeout(function() {
-                timereport.startTimer('timer3', 'group1');
-            }, 200);
-
-            setTimeout(function() {
-                timereport.endTimer('timer1', 'group1');
-                timereport.endTimer('timer2', 'group1');
-                timereport.endTimer('timer3', 'group1');
-
-                should(timereport.public_scope.groups['group1'].timers.length).equal(3);
-                done();
-            }, 300);
-
+        it('when calling timer func directly without group, timer should be placed in default group', function() {
+            tr.timer('d_t1').should.equal(tr.public_scope.groups['default'].timers['d_t1']);
         });
+
+        it('when calling timer with started id, it should return the timer object', function() {
+            var dt_1 = tr.timer('d_t1');
+            dt_1.should.equal(tr.public_scope.groups['default'].timers['d_t1']);
+        });
+
+        it('should be possible to start a timer inside a group', function() {
+            var g1_t1 = tr.group('g1').timer('g1_t1');
+            g1_t1.should.equal(tr.public_scope.groups['g1'].timers['g1_t1']);
+        });
+
+        it('should be possible to access a started timer from a group', function() {
+            var g1_t1 = tr.group('g1').timer('g1_t1');
+            g1_t1.should.equal(tr.public_scope.groups['g1'].timers['g1_t1']);
+        });
+
+
 
     });
 
-    describe('Colors', function() {
-
-        it('it should be possible to set colors on a group', function() {
-           timereport.setGroupColor(colors.red, 'testgroup1'); 
-        });
-
-    });
-
-    describe("Flush", function() {
-
-        it("Flush function should clear all groups and kill all timers.", function() {
-            //timereport.flush();
-        });
-
-    });
-
-    describe('Prints', function() {
-        it('should print the output in a nice way', function() {
-           timereport.printGroup('group1');
-        });
-    });
+    // describe('Output-time', function() {
+    //
+    //     it('should return x ms when less then 1 s', function() {
+    //         should(timereport.public_scope.outputTime(456)).equal("456 ms");
+    //     });
+    //
+    //     it('should return x s when more than 1 s and less than 1 m', function() {
+    //         should(timereport.public_scope.outputTime(23123)).equal("23.123 s");
+    //     });
+    //
+    //     it('should return x m and x s when more than 1 m but less than 1 h', function() {
+    //         should(timereport.public_scope.outputTime(60000*3+12345)).equal("3 m and 12.345 s");
+    //     });
+    //
+    //     it('should return a funny text because you really should not use this on that long processes.', function() {
+    //         should(timereport.public_scope.outputTime(60000*123), "looong time!");
+    //     });
+    //
+    // });
+    //
+    // describe('Groups', function() {
+    //
+    //     it('should create a new group when it is not created before', function() {
+    //         timereport.createGroup('testgroup1');
+    //         should(timereport.public_scope.groups['testgroup1'].id).equal('testgroup1');
+    //     });
+    //
+    //     it('should be possible to ad a color from colors string prototype as a base color', function() {
+    //        timereport.setColor(colors.red, 'testgroup1');
+    //     });
+    //
+    //     it('should create a group from startTimer functions if that group is not created before', function() {
+    //         timereport.startTimer('grouptimer1', 'specialgroup1');
+    //
+    //         timereport.endTimer('grouptimer1', 'specialgroup1');
+    //
+    //         should.exist(timereport.public_scope.groups['specialgroup1']);
+    //     });
+    //
+    //     it('should not create a new group if a group is already created', function() {
+    //         timereport.startTimer('testtimer', 'testgroup1');
+    //         timereport.startTimer('testtimer2', 'testgroup1');
+    //
+    //         timereport.endTimer('testtimer', 'testgroup1');
+    //         timereport.endTimer('testtimer2', 'testgroup1');
+    //
+    //         timereport.createGroup('testgroup1');
+    //
+    //         should(timereport.public_scope.groups['testgroup1'].timers.length).equal(2);
+    //     });
+    //
+    // });
+    //
+    // describe('Timers', function() {
+    //
+    //     it('should start a timer when a timer is started', function(done) {
+    //         timereport.startTimer('timer1', 'group1');
+    //
+    //         setTimeout(function() {
+    //             timereport.startTimer('timer2', 'group1');
+    //         }, 100);
+    //
+    //         setTimeout(function() {
+    //             timereport.startTimer('timer3', 'group1');
+    //         }, 200);
+    //
+    //         setTimeout(function() {
+    //             timereport.endTimer('timer1', 'group1');
+    //             timereport.endTimer('timer2', 'group1');
+    //             timereport.endTimer('timer3', 'group1');
+    //
+    //             should(timereport.public_scope.groups['group1'].timers.length).equal(3);
+    //             done();
+    //         }, 300);
+    //
+    //     });
+    //
+    // });
+    //
+    // describe('Colors', function() {
+    //
+    //     it('it should be possible to set colors on a group', function() {
+    //        timereport.setGroupColor(colors.red, 'testgroup1');
+    //     });
+    //
+    // });
+    //
+    // describe("Flush", function() {
+    //
+    //     it("Flush function should clear all groups and kill all timers.", function() {
+    //         //timereport.flush();
+    //     });
+    //
+    // });
+    //
+    // describe('Prints', function() {
+    //     it('should print the output in a nice way', function() {
+    //        timereport.printGroup('group1');
+    //     });
+    // });
 
 });
